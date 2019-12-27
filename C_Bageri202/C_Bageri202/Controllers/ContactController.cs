061bf7b5f93d7c66e77301ff21c1bc78d448ea56
Karin Skans <skka19st@student.ju.se>
2019-12-27 12:37:37 +0100

    databas_version01

---
+++ C_Bageri202/C_Bageri202/Controllers/ContactController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using C_Bageri202.Models;
+using C_Bageri202.ViewModels;
+
+namespace C_Bageri202.Controllers
+{
+    // koppling till Model-klass Contact
+    public class ContactController : Controller
+    {
+        // access till klass Contact via interfacet
+        private readonly IContact accessKontakt;
+
+        // constructor, indata är av typ interface 
+        // accessKontakt = lokal variabel
+        // inAccessKontakt = inkommande data
+        public ContactController(IContact inAccessKontakt)
+        {
+            accessKontakt = inAccessKontakt;
+        }
+
+        // action-metod Detail returnerar till view Detail
+        // finns enbart en kontakt, men Id=1 används vid 
+        // databas-anrop med hänsyn till primaryKey
+        public IActionResult Detail(int inId)
+        {
+            // skickar med fliknamnet till webben
+            ViewBag.Title = "Bageri 2.0";
+
+            // rubrik till webbsidan
+            ViewBag.Rubrik = "Kontakta oss";
+
+            // för att säkerställa värdet på id
+            inId = 1;
+            Contact kontakt = accessKontakt.GetContactById(inId);
+            if (kontakt == null)
+            {
+                // 404 - not found
+                return NotFound();
+            }
+
+            // skickar data till vyn Detail
+            return View(kontakt);
+        }
+    }
+}
---
+++ C_Bageri202/C_Bageri202/Models/Contact/ContactRepositoryMock.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace C_Bageri202.Models
+{
+    // kopplas ihop med interfacet för klassen Contact
+    public class ContactRepositoryMock : IContact
+    {
+        public Contact GetContactById(int inId)
+        {
+            return new Contact
+            {
+                Id = 1,
+                Name = "Baka på nätet",
+                WebbPage = "www.bakeonnet.se",
+                StreetAdress = "Bakagatan 1",
+                CityAdress = "755 90 Nätstaden",
+                Mail = "bakeonnet@gmail.com",
+                Phone = "012-34 56 789"
+            };
+        }
+    }
+}
